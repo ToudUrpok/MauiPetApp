2d048db3e5144fb70466aa3841b7fca2e9ac976d
y.yakubovich <y.yakubovich@softteco.com>
2023-11-14 18:11:02 +0300

    Add project files.

---
+++ MauiPetApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiPetApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiPetApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiPetApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
